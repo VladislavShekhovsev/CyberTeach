ce8d4193b98d79c607857a25503d016648c57663
Vladislav <vladislav13457@gmail.com>
2021-06-30 21:37:40 -0400

    Beginning of our project

---
+++ src/test/java/vlad/CyberTeach.java
@@ -0,0 +1,8 @@
+package vlad;
+
+public class CyberTeach {
+    public static void main(String[] args) {
+        System.out.println("Welcome to our Repo");
+        System.out.println("comment to create commit");
+    }
+}
